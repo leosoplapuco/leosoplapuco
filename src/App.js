87fe81e6d8c37a6e7d9a6bc4f0802d205ac310d6
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2026-07-13 23:29:28 -0500

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      leosoplapuco.com - sitio web en mantenimiento <a href='https://wa.link/2o5h2y' title='WhatsApp'>907057521</a>
     </div>
   );
 }
