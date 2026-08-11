0a17ad29be5848aaacb3937d80eedc9777fa4682
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2026-08-10 22:22:34 -0500

    0.0.7

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,14 @@
+import Header from '../../GeneralComponents/Header/Header';
 import Hero from './Components/Hero/Hero';
 
 import './Homepage.css';
 
 function Homepage(){
     return(
-        <Hero/>
+        <>
+            <Header/>
+            <Hero/>
+        </>
     )
 }
 
