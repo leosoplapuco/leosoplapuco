e8ab116f2f9af4ece5e9df93f2d90d9d83c2d9d9
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2026-08-11 22:09:32 -0500

    0.0.8

--- src/Pages/Homepage/Components/Hero/Hero.js
+++ src/Pages/Homepage/Components/Hero/Hero.js
@@ -4,19 +4,19 @@ function Hero(){
     return(
         <section className='hero-container'>
             <div className='hero-content'>
-                <span className='hero-content-span'>Desarrollador web</span>
-                <h1 className='hero-h1'>leosoplapuco</h1>
-                <p className='text-white text-center'>Impulsa tu negocio en internet con un sitio web a medida</p>
-
-                <div className='d-flex-center-center gap-10 margin-top-20'>
-                    <a href='/sobre-mi/' className='button-link button-link-3'>
-                        <span class="material-symbols-outlined">person</span>
-                        <p className='button-link-text'>Sobre mí</p>
-                    </a>
-                    <a href='mailto:leonardosoplapuco@gmail.com' className='button-link button-link-2'>
-                        <span class="material-symbols-outlined">mail</span>
-                        <p className='button-link-text'>Contactar</p>
-                    </a>
+                <div>
+                    <ul className='h-social-nets'>
+                        <li>
+                            <a href='/'>
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href='/'>
+                                <i class="fa-brands fa-telegram"></i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
